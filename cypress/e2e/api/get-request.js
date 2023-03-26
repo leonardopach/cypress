c181e98a25a4d05bbdf358f6bfd3e188f8a877e5
leonardopach <pachecodossantos17@gmail.com>
2023-03-25 21:00:40 -0300

    cypress

---
+++ cypress/e2e/api/get-request.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress"/>
+
+describe('Get Request', () => {
+
+    let result;
+    it('Validate status code of the /posts api', () => {
+        result = cy.request("http://localhost:3000/posts")
+            .its("status").should("equal", 200);
+    });
+
+    it("Validate /posts api contains the correct keys and values", () => {
+        cy.request({
+            method: 'GET',
+            url: 'http://localhost:3000/posts',
+            headers: {
+                accept: "application/json"
+            }
+        }).then(response => {
+            let body = JSON.parse(JSON.stringify(response.body))
+            expect(body[0]).has.property("title", "json-server");
+            cy.log(body);
+
+            body.forEach((item) => {
+                expect(item).to.have.all.keys("id", "title", "author");
+                cy.log(`Autor ${item["author"]}, & Title ${item["title"]}`);
+            })
+        });
+
+    })
+});
---
+++ cypress/e2e/api/post-requests.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress"/>
+
+describe('Post Request', () => {
+    let titleOfPosts = new Array();
+    it('Create a new post via /posts api', () => {
+        cy.request({
+            method: "POST",
+            url: 'http://localhost:3000/posts',
+            body: {
+                title: "Want to learn automation testing?",
+                author: "Sarah Jones"
+            }
+        }).then(response => {
+            expect(response.status).to.eql(201);
+        })
+    });
+
+    it("Validate title of latest post", () => {
+        cy.request({
+            method: 'GET',
+            url: 'http://localhost:3000/posts',
+            headers: {
+                accept: "application/json"
+            }
+        }).then(response => {
+            let body = JSON.parse(JSON.stringify(response.body));
+            body.forEach(item => {
+                titleOfPosts.push(item["title"]);
+            });
+        })
+    })
+});
